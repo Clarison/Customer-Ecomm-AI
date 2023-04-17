19dc1bdc66e2360d22d01104da21640e7c61393d
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-04-17 19:00:59 -0400

    Update 1_Customer_Lifetime_Spend_Prediction.py

--- pages/1_Customer_Lifetime_Spend_Prediction.py
+++ pages/1_Customer_Lifetime_Spend_Prediction.py
@@ -1,3 +1,23 @@
 import streamlit as st
+import snowflake.connector
 
-st.title("This is Customer Lifetime spend with our Organisation")
+# Connect to Snowflake
+conn = snowflake.connector.connect(
+    user='alekyakastury',
+    password='@Noon1240',
+    account='dl84836.us-east-2.aws',
+    warehouse='COMPUTE_WH',
+    database='SNOWFLAKE_SAMPLE_DATA',
+    schema='TPCH_SF10'
+)
+
+# Define a SQL query to fetch data from a table
+query = 'SELECT * FROM customer limit 10'
+
+# Execute the query and fetch the results
+cur = conn.cursor()
+cur.execute(query)
+results = cur.fetchall()
+
+# Display the results in Streamlit
+st.write(results)
