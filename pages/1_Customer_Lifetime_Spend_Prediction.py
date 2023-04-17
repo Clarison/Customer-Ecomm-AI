5f649bde629adfdb5b5362e2b879f82c95da17ea
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-04-17 19:04:09 -0400

    Update 1_Customer_Lifetime_Spend_Prediction.py

--- pages/1_Customer_Lifetime_Spend_Prediction.py
+++ pages/1_Customer_Lifetime_Spend_Prediction.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import snowflake.connector
+import pandas as pd
 
 # Connect to Snowflake
 conn = snowflake.connector.connect(
@@ -18,6 +19,7 @@ query = 'SELECT * FROM customer limit 10'
 cur = conn.cursor()
 cur.execute(query)
 results = cur.fetchall()
+df = pd.DataFrame(results, columns=[i[0] for i in cur.description])
 
 # Display the results in Streamlit
-st.write(results)
+st.write(df)
