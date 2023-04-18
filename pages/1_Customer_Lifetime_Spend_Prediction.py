c4581eab7b977422f1bec31226fa2b8c18b478b1
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-04-17 22:48:17 -0400

    another chache

--- pages/1_Customer_Lifetime_Spend_Prediction.py
+++ pages/1_Customer_Lifetime_Spend_Prediction.py
@@ -33,46 +33,49 @@ st.write(df.head())
 
 #selecting only required columns
 #df= df[['O_CUSTKEY','O_ORDERKEY','O_TOTALPRICE','O_ORDERDATE']]
+@st.cache(allow_output_mutation=True)
+def get_data_cleaned(query):
+    # Drop Null values
+    df.dropna(subset=['O_CUSTKEY'], inplace=True)
 
-# Drop Null values
-df.dropna(subset=['O_CUSTKEY'], inplace=True)
 
 
 
+    # Sort by Age in ascending order
+    df_orders = df.sort_values('O_CUSTKEY')
 
-# Sort by Age in ascending order
-df_orders = df.sort_values('O_CUSTKEY')
+    # Display the results in Streamlit
+    #st.write(df_orders)
 
-# Display the results in Streamlit
-#st.write(df_orders)
+    # total amount of purchases by each customer
+    def groupby_mean(x):
+        return x.mean()
 
-# total amount of purchases by each customer
-def groupby_mean(x):
-    return x.mean()
+    def groupby_count(x):
+        return x.count()
 
-def groupby_count(x):
-    return x.count()
+    def purchase_duration(x):
+        return (x.max() - x.min()).days
 
-def purchase_duration(x):
-    return (x.max() - x.min()).days
+    def avg_frequency(x):
+        return (x.max() - x.min()).days / x.count()
 
-def avg_frequency(x):
-    return (x.max() - x.min()).days / x.count()
+    groupby_mean.__name__ = 'avg'
+    groupby_count.__name__ = 'count'
+    purchase_duration.__name__ = 'purchase_duration'
+    avg_frequency.__name__ = 'purchase_frequency'
 
-groupby_mean.__name__ = 'avg'
-groupby_count.__name__ = 'count'
-purchase_duration.__name__ = 'purchase_duration'
-avg_frequency.__name__ = 'purchase_frequency'
+    df_summary = df_orders.reset_index().groupby('O_CUSTKEY').agg({
+                'O_TOTALPRICE': [min, max, sum, groupby_mean, groupby_count],
+                'O_ORDERDATE': [min, max, purchase_duration, avg_frequency]
+                 })
+    df_summary.columns = ['_'.join(col).lower() for col in df_summary.columns]
+    #df_summary = df_summary.loc[df_summary['invoicedate_purchase_duration'] > 0]
 
-df_summary = df_orders.reset_index().groupby('O_CUSTKEY').agg({
-            'O_TOTALPRICE': [min, max, sum, groupby_mean, groupby_count],
-            'O_ORDERDATE': [min, max, purchase_duration, avg_frequency]
-             })
-df_summary.columns = ['_'.join(col).lower() for col in df_summary.columns]
-#df_summary = df_summary.loc[df_summary['invoicedate_purchase_duration'] > 0]
+    df_summary = df_summary.sort_values('o_totalprice_count', ascending=False)
+    return df_summary
 
-df_summary = df_summary.sort_values('o_totalprice_count', ascending=False)
-
+df_summary=get_data_cleaned(df)
 # Display the results in Streamlit
 #st.write(df_summary)
 
