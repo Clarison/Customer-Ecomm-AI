0f46dc63e9d909dec2663a6409a0b7c667b3713d
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-04-23 22:19:13 -0400

    Update 2_Targeted_audiance.py

--- pages/2_Targeted_audiance.py
+++ pages/2_Targeted_audiance.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import snowflake.connector
+import pandas as pd
+import altair as alt
 
+# Connect to Snowflake
+conn = snowflake.connector.connect(
+    user= st.secrets["user"],
+    password= st.secrets["password"],
+    account= st.secrets["account"],
+    warehouse= st.secrets["warehouse"],
+    database= st.secrets["database"],
+    schema= st.secrets["schema"]
+)
+
+    
 st.write("Hello, world!")
