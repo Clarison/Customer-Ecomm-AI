38958b846f1041f45de3ea3abe1b9cc75b6ad71d
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-04-24 18:31:10 -0400

    Update 3_Product_Recomendation.py

--- pages/3_Product_Recomendation.py
+++ pages/3_Product_Recomendation.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import snowflake.connector
+import pandas as pd
+import altair as alt
 
+# Connect to Snowflake
+conn = snowflake.connector.connect(
+    user= st.secrets["user"],
+    password= st.secrets["password"],
+    account= st.secrets["account"],
+    warehouse= st.secrets["warehouse"],
+    database= st.secrets["database"],
+    schema= st.secrets["schema"]
+)
+
+st.write(Recomended Products Pairs)
