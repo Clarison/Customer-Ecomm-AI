364ae5c85037c588a9be2fe6d25966a80e26267d
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-04-24 18:33:12 -0400

    Update 3_Product_Recomendation.py

--- pages/3_Product_Recomendation.py
+++ pages/3_Product_Recomendation.py
@@ -14,3 +14,19 @@ conn = snowflake.connector.connect(
 )
 
 st.write('Recomended Products Pairs')
+
+# Define a SQL query to fetch data from a table
+query = 'select * from product_recom'
+
+# Execute the query and fetch the results into a DataFrame
+@st.cache_data()
+def get_data_from_snowflake(query):
+    cur = conn.cursor()
+    cur.execute(query)
+    results = cur.fetchall()
+    df = pd.DataFrame(results, columns=[i[0] for i in cur.description])
+    return df
+
+df = get_data_from_snowflake(query)
+
+st.write(df.head())
