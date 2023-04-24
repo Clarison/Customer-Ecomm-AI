4033a55df5637c4d4a71f0cb8b06059994ed2c02
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-04-23 22:21:21 -0400

    Update 2_Targeted_audiance.py

--- pages/2_Targeted_audiance.py
+++ pages/2_Targeted_audiance.py
@@ -13,5 +13,22 @@ conn = snowflake.connector.connect(
     schema= st.secrets["schema"]
 )
 
+# Define a SQL query to fetch data from a table
+query = 'select ss_item_sk,count(*) from store_sales_new group by ss_item_sk order by 2 desc limit 50 '
+
+# Execute the query and fetch the results into a DataFrame
+@st.cache_data()
+def get_data_from_snowflake(query):
+    cur = conn.cursor()
+    cur.execute(query)
+    results = cur.fetchall()
+    df = pd.DataFrame(results, columns=[i[0] for i in cur.description])
+    return df
+
+df = get_data_from_snowflake(query)
+
+# Display the results in Streamlit
+st.write(df.head())
+
     
 st.write("Hello, world!")
