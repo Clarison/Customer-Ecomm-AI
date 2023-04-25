bfcd64e32d022d6fbc15329e700d615af8a5218f
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-04-24 22:31:45 -0400

    Update 3_Product_Recomendation.py

--- pages/3_Product_Recomendation.py
+++ pages/3_Product_Recomendation.py
@@ -94,7 +94,7 @@ product_b_str ='(' + ','.join(map(str, product_b)) + ')'
 st.write(product_b_str)
 st.write("You may also like to look at before you complete your purchase :")
 
-query = f"SELECT I_ITEM_ID,I_PRODUCT_NAME,I_CLASS,I_CATEGORY,I_ITEM_DESC FROM Item WHERE i_item_sk in '{product_b_str}'"
+query = f"SELECT I_ITEM_ID,I_PRODUCT_NAME,I_CLASS,I_CATEGORY,I_ITEM_DESC FROM Item WHERE i_item_sk in {product_b_str}"
 
 # Execute the query
 df = get_data_from_snowflake(query)
