ea5f4fa3851ffd2d709dd2c926a427076cc101c6
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-04-26 17:57:21 -0400

    Update 1_Customer_Lifetime_Spend_Prediction.py

--- pages/1_Customer_Lifetime_Spend_Prediction.py
+++ pages/1_Customer_Lifetime_Spend_Prediction.py
@@ -28,6 +28,16 @@ def get_data_from_snowflake(query):
 st.write("<h1 style='text-align: center;'>Customers LifeTime Value</h1>", unsafe_allow_html=True)
 
 
+# Set page title and description
+st.set_page_config(page_title="Customer Lifetime Value Calculation", page_icon=":dollar:", layout="wide")
+st.title("Customer Lifetime Value Calculation")
+st.write("In this section, we are using customer transaction data to calculate individual customer lifetime value (CLV). By estimating the total revenue a business can expect from a single customer over their lifetime, we can determine the value of each customer and make informed decisions about marketing, sales, and customer service strategies to increase profitability.")
+
+# Add some padding and a separator
+st.write("")
+st.write("---")
+st.write("")
+
 df = get_data_from_snowflake(query)
 
 
