29135f536d464ac35d2304a3c35ac6b3815ed159
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-04-26 00:08:48 -0400

    Update 3_Product_Recomendation.py

--- pages/3_Product_Recomendation.py
+++ pages/3_Product_Recomendation.py
@@ -102,3 +102,10 @@ st.write(df)
 
 st.title("Context Based Search")
 
+query = 'select i_item_sk,i_class,i_category,i_color from item limit 10'
+
+# Execute the query
+df = get_data_from_snowflake(query)
+# Display the result
+st.write(df)
+
